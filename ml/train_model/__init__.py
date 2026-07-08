60582931eb77452be340e842631e2353c1216918
Mahesh Nagapuri <maheshnagapuri547@gmail.com>
2026-07-08 14:50:03 +0530

    Delete ml/train_model/__init__.py

--- ml/train_model/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Model training module for ML pipeline - Train quality prediction models."""
-
-from .train_defect_classifier import train_defect_classifier
-from .train_return_rate_predictor import train_return_rate_predictor
-
-__all__ = [
-    'train_defect_classifier',
-    'train_return_rate_predictor',
-]
